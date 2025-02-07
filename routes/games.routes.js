80dae6a6c4ddcfa7124fb7e11f93af00d93411d7
Erian Flores <erian.flores@gmail.com>
2025-02-07 01:37:30 +0100

    /search added to game.routes

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -35,47 +35,62 @@ router.get("/fetch-games", async (req, res) => {
 router.get("/featured", async (req, res) => {
   console.log("/featured route hit");
   try {
-    const featuredGames = await Game.aggregate([{$sample: { size: 10 } }]);
+    const featuredGames = await Game.aggregate([{ $sample: { size: 10 } }]);
     res.json(featuredGames);
   } catch (error) {
     console.error("Error fetching featured games:", error.message);
   }
 });
 
-
-
 router.get("/top-games", async (req, res) => {
-  console.log("Fetching top-rated games with IDs:"); 
-    try {
-        const topRatedGameIds = [3328, 5286, 12020, 5679]; 
-        let topGames = [];
+  console.log("Fetching top-rated games with IDs:");
+  try {
+    const topRatedGameIds = [3328, 5286, 12020, 5679];
+    let topGames = [];
 
-        console.log("Fetching top-rated games with IDs:", topRatedGameIds);
+    console.log("Fetching top-rated games with IDs:", topRatedGameIds);
 
-        
-        for (let gameId of topRatedGameIds) {
-            console.log("Fetching game with ID:", gameId);  
-            const game = await Game.findOne({ id: Number(gameId) });
+    for (let gameId of topRatedGameIds) {
+      console.log("Fetching game with ID:", gameId);
+      const game = await Game.findOne({ id: Number(gameId) });
 
-            if (game) {
-                topGames.push(game);
-            } else {
-                console.log(`Game with ID ${gameId} not found.`);
-            }
-        }
-
-        console.log("Fetched Games:", topGames); 
-
-        if (topGames.length === 0) {
-            return res.status(404).json({ message: "No top-rated games found" });
-        }
-
-        
-        res.status(200).json(topGames);
-    } catch (error) {
-        console.error("Error fetching top-rated games:", error);
-        res.status(500).json({ error: "Failed to fetch top-rated games" });
+      if (game) {
+        topGames.push(game);
+      } else {
+        console.log(`Game with ID ${gameId} not found.`);
+      }
     }
+
+    console.log("Fetched Games:", topGames);
+
+    if (topGames.length === 0) {
+      return res.status(404).json({ message: "No top-rated games found" });
+    }
+
+    res.status(200).json(topGames);
+  } catch (error) {
+    console.error("Error fetching top-rated games:", error);
+    res.status(500).json({ error: "Failed to fetch top-rated games" });
+  }
+});
+
+router.get("/search", async (req, res) => {
+  try {
+    const { query } = req.query;
+    if (!query) {
+      return res.status(400).json({ message: "No search query provided" });
+    }
+
+    const games = await Game.find(
+      { name: { $regex: query, $options: "i" } },
+      "-id"
+    );
+
+    res.json(games);
+  } catch (error) {
+    console.log("Search error:", error);
+    res.status(500).json({ message: "Error searching games" });
+  }
 });
 
 // Route to fetch details of a single game by ID
