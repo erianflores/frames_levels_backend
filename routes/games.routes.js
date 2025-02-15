a7ab381a73ea25a51a8ef09430e5b5ffd56d33e8
Erian Flores <erian.flores@gmail.com>
2025-02-15 11:09:10 +0100

    One-game route modified 2

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -115,9 +115,9 @@ router.get("/one-game/:id", async (req, res) => {
   try {
     const game = await Game.findById(req.params.id);
 
-    // if (!game) {
-    //   return res.status(500).json({ message: "Game not found" });
-    // }
+    if (!game) {
+      return res.status(500).json({ message: "Game not found" });
+    }
 
     res.json(game);
   } catch (error) {
